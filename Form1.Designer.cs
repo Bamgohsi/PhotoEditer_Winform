36b3c11deb15b885e71da787541f011d15262750
모블 <@DESKTOP-K346EVR>
2025-08-02 08:47:19 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace photo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            tabPage2 = new TabPage();
+            pictureBox1 = new PictureBox();
+            tabControl1 = new TabControl();
+            menuStrip1 = new MenuStrip();
+            toolStrip_File = new ToolStripMenuItem();
+            toolStrip_NewFile = new ToolStripMenuItem();
+            toolStrip_Open = new ToolStripMenuItem();
+            toolStripp_Save = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            groupBox2 = new GroupBox();
+            btn_Save = new Button();
+            btn_NewFile = new Button();
+            btn_Open = new Button();
+            groupBox1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(12, 80);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(106, 358);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(54, 186);
+            button10.Name = "button10";
+            button10.Size = new Size(42, 35);
+            button10.TabIndex = 0;
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button4_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(6, 186);
+            button9.Name = "button9";
+            button9.Size = new Size(42, 35);
+            button9.TabIndex = 0;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(54, 145);
+            button8.Name = "button8";
+            button8.Size = new Size(42, 35);
+            button8.TabIndex = 0;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button4_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(6, 145);
+            button7.Name = "button7";
+            button7.Size = new Size(42, 35);
+            button7.TabIndex = 0;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(54, 104);
+            button6.Name = "button6";
+            button6.Size = new Size(42, 35);
+            button6.TabIndex = 0;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(6, 104);
+            button5.Name = "button5";
+            button5.Size = new Size(42, 35);
+            button5.TabIndex = 0;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(54, 63);
+            button4.Name = "button4";
+            button4.Size = new Size(42, 35);
+            button4.TabIndex = 0;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(6, 63);
+            button3.Name = "button3";
+            button3.Size = new Size(42, 35);
+            button3.TabIndex = 0;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(54, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(42, 35);
+            button2.TabIndex = 0;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(6, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(42, 35);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(pictureBox1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(581, 330);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(61, 28);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(472, 427);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(134, 71);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(589, 358);
+            tabControl1.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { toolStrip_File, toolStripMenuItem3 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStrip_File
+            // 
+            toolStrip_File.DropDownItems.AddRange(new ToolStripItem[] { toolStrip_NewFile, toolStrip_Open, toolStripp_Save });
+            toolStrip_File.Name = "toolStrip_File";
+            toolStrip_File.Size = new Size(43, 20);
+            toolStrip_File.Text = "파일";
+            // 
+            // toolStrip_NewFile
+            // 
+            toolStrip_NewFile.Name = "toolStrip_NewFile";
+            toolStrip_NewFile.ShortcutKeys = Keys.Control | Keys.N;
+            toolStrip_NewFile.Size = new Size(181, 22);
+            toolStrip_NewFile.Text = "새로 만들기";
+            // 
+            // toolStrip_Open
+            // 
+            toolStrip_Open.Name = "toolStrip_Open";
+            toolStrip_Open.ShortcutKeys = Keys.Control | Keys.O;
+            toolStrip_Open.Size = new Size(181, 22);
+            toolStrip_Open.Text = "열기";
+            // 
+            // toolStripp_Save
+            // 
+            toolStripp_Save.Name = "toolStripp_Save";
+            toolStripp_Save.ShortcutKeys = Keys.Control | Keys.S;
+            toolStripp_Save.Size = new Size(181, 22);
+            toolStripp_Save.Text = "저장하기";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(127, 20);
+            toolStripMenuItem3.Text = "toolStripMenuItem3";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btn_Save);
+            groupBox2.Controls.Add(btn_NewFile);
+            groupBox2.Controls.Add(btn_Open);
+            groupBox2.Location = new Point(12, 24);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(776, 41);
+            groupBox2.TabIndex = 3;
+            groupBox2.TabStop = false;
+            // 
+            // btn_Save
+            // 
+            btn_Save.Location = new Point(84, 12);
+            btn_Save.Name = "btn_Save";
+            btn_Save.Size = new Size(28, 23);
+            btn_Save.TabIndex = 0;
+            btn_Save.Text = "저";
+            btn_Save.UseVisualStyleBackColor = true;
+            btn_Save.Click += btn_Save_Click;
+            // 
+            // btn_NewFile
+            // 
+            btn_NewFile.Location = new Point(20, 12);
+            btn_NewFile.Name = "btn_NewFile";
+            btn_NewFile.Size = new Size(28, 23);
+            btn_NewFile.TabIndex = 0;
+            btn_NewFile.Text = "새";
+            btn_NewFile.UseVisualStyleBackColor = true;
+            btn_NewFile.Click += btn_NewFile_Click;
+            // 
+            // btn_Open
+            // 
+            btn_Open.Location = new Point(50, 12);
+            btn_Open.Name = "btn_Open";
+            btn_Open.Size = new Size(28, 23);
+            btn_Open.TabIndex = 0;
+            btn_Open.Text = "옾";
+            btn_Open.UseVisualStyleBackColor = true;
+            btn_Open.Click += btn_Open_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox2);
+            Controls.Add(tabControl1);
+            Controls.Add(groupBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private TabPage tabPage2;
+        private TabControl tabControl1;
+        private Button button12;
+        private Button btn_Road;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem toolStrip_File;
+        private ToolStripMenuItem toolStrip_Open;
+        private ToolStripMenuItem toolStripp_Save;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private GroupBox groupBox2;
+        private Button btn_Open;
+        private ToolStripMenuItem toolStrip_NewFile;
+        private Button btn_Save;
+        private Button btn_NewFile;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace photo
+{
+    public partial class Form1 : Form
+    {
+        // 이미지 드래그 중 여부를 나타내는 플래그
+        private bool isDragging = false;
+
+        // 드래그 시작 시 마우스 클릭 지점 좌표
+        private Point clickOffset;
+
+        // 선택 테두리를 표시할지 여부 (마우스 클릭 시 true)
+        private bool showSelectionBorder = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // pictureBox1에 커스텀 그리기(Paint) 이벤트 연결
+            pictureBox1.Paint += pictureBox1_Paint;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // 미사용 버튼 - 추후 기능 연결 가능
+        }
+
+        // [새로 만들기] 버튼 클릭 시 실행
+        // pictureBox의 이미지 초기화
+        private void btn_NewFile_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        // [열기] 버튼 클릭 시 실행
+        // 이미지 파일을 선택하고 pictureBox에 로드
+        private void btn_Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "이미지 열기";
+            openFileDialog.Filter = "이미지 파일|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // 기존 이미지가 있을 경우 메모리 해제
+                    pictureBox1.Image?.Dispose();
+
+                    // 새로운 이미지 로드
+                    Image img = Image.FromFile(openFileDialog.FileName);
+                    pictureBox1.Image = img;
+
+                    // 이미지 크기에 맞게 PictureBox 크기 조절
+                    pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox1.Size = img.Size;
+
+                    // pictureBox 위치 설정 (좌측 상단 여백)
+                    pictureBox1.Location = new Point(10, 10);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("이미지를 불러오는 중 오류 발생: " + ex.Message);
+                }
+            }
+        }
+
+        // [저장] 버튼 클릭 시 실행 (추후 구현 예정)
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            // TODO: 저장 기능 구현
+        }
+
+        // 마우스 버튼을 누를 때 호출됨
+        // 드래그 시작 처리 및 선택 테두리 표시
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (pictureBox1.Image != null && e.Button == MouseButtons.Left)
+            {
+                isDragging = true;          // 드래그 시작
+                clickOffset = e.Location;   // 마우스 클릭 좌표 저장
+                showSelectionBorder = true; // 테두리 표시 ON
+                pictureBox1.Invalidate();   // 다시 그리기 요청 (Paint 호출)
+            }
+        }
+
+        // 마우스를 이동할 때 호출됨 (드래그 중일 때만)
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // 기존 위치에서 마우스 이동만큼 offset 적용
+                Point newLocation = pictureBox1.Location;
+                newLocation.X += e.X - clickOffset.X;
+                newLocation.Y += e.Y - clickOffset.Y;
+
+                // PictureBox 위치 갱신
+                pictureBox1.Location = newLocation;
+            }
+        }
+
+        // 마우스 버튼을 놓을 때 호출됨
+        // 드래그 종료 및 선택 테두리 해제
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDragging = false;
+
+            // 선택 해제하고 싶을 경우 주석 해제
+            showSelectionBorder = false;
+
+            // 다시 그리기 요청 (Paint 호출)
+            pictureBox1.Invalidate();
+        }
+
+        // 폼 로드 시 실행 (필요 시 초기화 처리 가능)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 현재는 비어 있음
+        }
+
+        // pictureBox1이 다시 그려질 때 호출됨
+        // 선택 테두리를 그림
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (showSelectionBorder)
+            {
+                using (Pen pen = new Pen(Color.DeepSkyBlue, 2))
+                {
+                    // 실선으로 테두리 그리기 (점선은 DashStyle.Dot 등 사용 가능)
+                    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+
+                    // 그림 테두리 사각형 정의 (이미지 전체)
+                    Rectangle rect = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+
+                    // 테두리 그리기
+                    e.Graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+    }
+}
