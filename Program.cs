8d7dc68ccf4a24f5ac973b606db5d11f63094c2a
Bamgohsi <pkpkpp87@gmail.com>
2025-08-02 16:43:49 +0900

    dddd

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ namespace photo
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            ////
         }
     }
 }
